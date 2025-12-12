5890971bbc9e17404c820dd8765963b3c090b546
madinak07 <252433@astanait.edu.kz>
2025-12-12 12:45:03 +0500

    ASSIGNMENT#1_MADINA_HOSPITAL_IT-2504

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    System.out.println("Hello and welcome!");
-    System.out.println("Madina crash");
+    System.out.println("Welcome to my first Project!");
+    System.out.println("This code is about the Hospital");
+    System.out.println("The main features are: save information about patient, help to make an appointment, and give example diagnosis");
+    System.out.println("My name is Kairbayeva Madina, IT-2504");
+
 }
